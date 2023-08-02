4dd598f7d16bc34aace8c06ca4b41c17d959a45d
kimdoyup <gltlvl12@gmail.com>
2023-08-02 23:08:03 +0900

    temp

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,85 @@
 import 'dart:async';
 
 import 'package:diary/app.dart';
+import 'package:diary/ui/vm/home_view_model.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'firebase_options.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.notification!.body}");
+
+  // showFlutterNotification(message);
+}
+//
+// /// Create a [AndroidNotificationChannel] for heads up notifications
+// late AndroidNotificationChannel channel;
+//
+// bool isFlutterLocalNotificationsInitialized = false;
+//
+// Future<void> setupFlutterNotifications() async {
+//   if (isFlutterLocalNotificationsInitialized) {
+//     return;
+//   }
+//   channel = const AndroidNotificationChannel(
+//     'high_importance_channel', // id
+//     'High Importance Notifications', // title
+//     description:
+//     'This channel is used for important notifications.', // description
+//     importance: Importance.high,
+//   );
+//
+//   flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+//
+//   /// Create an Android Notification Channel.
+//   ///
+//   /// We use this channel in the `AndroidManifest.xml` file to override the
+//   /// default FCM channel to enable heads up notifications.
+//   await flutterLocalNotificationsPlugin
+//       .resolvePlatformSpecificImplementation<
+//       AndroidFlutterLocalNotificationsPlugin>()
+//       ?.createNotificationChannel(channel);
+//
+//   /// Update the iOS foreground notification presentation options to allow
+//   /// heads up notifications.
+//   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+//     alert: true,
+//     badge: true,
+//     sound: true,
+//   );
+//   isFlutterLocalNotificationsInitialized = true;
+// }
+//
+// void showFlutterNotification(RemoteMessage message) {
+//   RemoteNotification? notification = message.notification;
+//   AndroidNotification? android = message.notification?.android;
+//   if (notification != null && android != null && !kIsWeb) {
+//     showFlutterNotification(message);
+//     flutterLocalNotificationsPlugin.show(
+//       notification.hashCode,
+//       notification.title,
+//       notification.body,
+//       NotificationDetails(
+//         android: AndroidNotificationDetails(
+//           channel.id,
+//           channel.name,
+//           channelDescription: channel.description,
+//           // TODO add a proper drawable resource to android, for now using
+//           //      one that already exists in example app.
+//           icon: 'launch_background',
+//         ),
+//       ),
+//     );
+//   }
+// }
+// late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
 void main() async {
   runZonedGuarded(() async {
@@ -17,7 +89,18 @@ void main() async {
     // Firebase
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform);
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+    // FirebaseMessaging.instance.requestPermission(
+    //   alert: true,
+    //   announcement: true,
+    //   badge: true,
+    //   carPlay: true,
+    //   criticalAlert: true,
+    //   provisional: true,
+    //   sound: true,
+    // );
+    FirebaseMessaging.onBackgroundMessage((message) =>
+        _firebaseMessagingBackgroundHandler(message)
+    );
 
     runApp(const ProviderScope(child: App()));
   }, (error, stack) {
@@ -25,6 +108,3 @@ void main() async {
   });
 }
 
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print("Handling a background message: ${message.notification!.body}");
-}
